8e40ec2b2f098ef8c524d9bce11e133560caf1b3
pitkant <69813611+pitkant@users.noreply.github.com>
2020-08-28 10:14:33 +0300

    Implementation of p.temp functionality, added a while loop to prevent extremely rare but invalid individual numbers "000" and "001" from being generated

--- R/rpin.R
+++ R/rpin.R
@@ -8,7 +8,7 @@
 #' @param start_date Lower limit of generated \code{hetu} dates. Default is 1895-01-01.
 #' @param end_date Upper limit of generated \code{hetu}. Default is the current date.
 #' @param p.male Proportion of males. Default is 0.3.
-#' @param p.temp Proportion of temporary identification numbers. Default is 0.1. Currently not implemented.
+#' @param p.temp Proportion of temporary identification numbers. Default is 0.1.
 #' 
 #' @return a vector of generated \code{hetu}-pins.
 #' 
@@ -21,23 +21,41 @@
 #' @export
 rpin <- function(n, start_date = as.Date("1895-01-01"), end_date = Sys.Date(), p.male = 0.3, p.temp = 0.1){
   rdate <- sample(start_date:end_date, n, replace = TRUE)
+  # origin date according to POSIX standard
   rdate <- as.Date(rdate, origin = "1970-01-01")
+  
   ddmmyy <- format(rdate, "%d%m%y")
+  
+  # determine the correct century marker
   century <- function(x) {
     switch(substr(x, 1, 2),
          "20" = "A",
          "19" = "-",
          "18" = "+",
-         stop("Invalid `x` value")
+         stop("Invalid date input")
     )
   }
-  sapply(rdate, century)
-  zz <- sample(0:89, n, replace = TRUE)
-  zz <- formatC(zz, width = 2, format = "d", flag = "0")
-  z <- sample(x = as.character(c(2, 4, 6, 8, 3, 5, 7, 9)), 
-            prob = c(rep(1 - p.male, 4), rep(p.male, 4)), replace = TRUE, 
-            size = n)
-  checklist <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "U", "V", "W", "X", "Y")
+  
+  test_zzz <- "000"
+  while ("000" %in% test_zzz | "001" %in% test_zzz) {
+    zz_norm <- sample(x = 0:89, replace = TRUE, size = round(n*(1-p.temp)))
+    zz_temp <- sample(x = 90:99, replace = TRUE, size = round(n*p.temp))
+    zz <- append(zz_norm, zz_temp)
+    #randomize order of pins to make the vector seem more natural
+    zz <- sample(zz)
+    zz <- formatC(zz, width = 2, format = "d", flag = "0")
+    z <- sample(x = as.character(c(0, 2, 4, 6, 8, 1, 3, 5, 7, 9)), 
+                prob = c(rep(1 - p.male, 5), rep(p.male, 5)), replace = TRUE, 
+                size = n)
+    test_zzz <- paste0(zz,z)
+  }
+  
+  # characters used for determining the checksum of hetu
+  checklist <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+                 "A", "B", "C", "D", "E", "F", "H", "J", "K", "L", 
+                 "M", "N", "P", "R", "S", "T", "U", "V", "W", "X", "Y")
+  
+  # "+ 1" is necessary as checklist[0] returns nothing
   checksum <- checklist[as.numeric(paste0(ddmmyy, zz, z)) %% 31 + 1]
   paste0(ddmmyy, sapply(rdate, century), zz, z, checksum)
 }
