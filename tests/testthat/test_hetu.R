44f3f1b1785585ba744ac6fe7334ed831dc74cf3
pitkant <69813611+pitkant@users.noreply.github.com>
2020-09-02 09:22:08 +0300

    Added tests for 100 % test coverage

--- tests/testthat/test_hetu.R
+++ tests/testthat/test_hetu.R
@@ -1,7 +1,10 @@
 context("Hetu (personal identification number in Finland")
 
-test_that("hetu works correctly", {
+test_that("hetu() works correctly", {
+  expect_error(hetu("010101-0101", extract = ""))
   expect_equal(hetu(c("010101-0101", "111111-111C"), extract = "sex"), c("Female", "Male"))
+  expect_true(typeof(hetu(c("010101-0101", "111111-111C"), extract = NULL)) == "list")
+  expect_true(is.na(hetu(010101-0101))) #convert to character vector, check general format
   expect_equal(hetu(c("111111-111C"), extract = "sex"), "Male")
   expect_equal(as.character(hetu("010101-0101")$hetu), "010101-0101")
   expect_equal(as.character(hetu(c("010101-0101"))$sex), "Female")
@@ -11,18 +14,45 @@ test_that("hetu works correctly", {
   expect_equal(hetu("010101-0101")$day, 1)
   expect_equal(hetu("010101-0101")$month, 1)
   expect_equal(hetu("010101-0101")$year, 1901)
+  expect_true(is.na(hetu("320101-010A"))) #Check day
+  expect_true(is.na(hetu("011301-010P"))) #Check month
+  expect_true(is.na(hetu("0101-1-0101"))) #Check year
+  expect_true(is.na(hetu("010101B0101"))) #Check century
+  expect_true(is.na(hetu("290201-010A"))) #Check if date exists
+  expect_equal(as.character(hetu("010199+010A")$century.char), "+")
   expect_equal(as.character(hetu("010101-0101")$century.char), "-")
+  expect_equal(as.character(hetu("010101A0101")$century.char), "A")
+  expect_true(is.na(hetu("010101-000G"))) #Check checksum character validity
+  expect_true(is.na(hetu("010101-000P", allow.temp = TRUE))) #Check personal identification number
+})
 
-  expect_true(pin_ctrl("010101-0101"))
+test_that("pin_ctrl() works correctly", {
+  expect_true(all(pin_ctrl(c("010101-0101", "111111-111C"))))
   expect_false(pin_ctrl("010101-010A"))
-  expect_true(pin_ctrl("010101-900R", allow.temp = TRUE))
-  expect_false(pin_ctrl("010101-900R", allow.temp = FALSE))
+  expect_true(pin_ctrl("010101A900R", allow.temp = TRUE))
+  expect_false(pin_ctrl("010101A900R", allow.temp = FALSE))
+})
   
+test_that("pin_to_date() works correctly", {
   expect_true(all((pin_to_date(c("010101-0101", "111111-111C")) == c("1901-01-01", "1911-11-11"))))
   expect_true(all((pin_to_date(c("010101A0101", "111111A111C")) == c("2001-01-01", "2011-11-11"))))
-  
+})
+ 
+test_that("pin_age() works correctly", { 
   expect_true(pin_age("010101-0101", date = Sys.Date()) > 100)
-  
+  expect_true(all(pin_age(c("010101-0101", "111111-111C"), date = Sys.Date()) > 100))
+  expect_equal(pin_age(c("010101-0101", "111111-111C"), date = c("2000-01-01", "2010-01-01")), c(99,98))
+  expect_error(pin_age(c("010101-0101"), date = c("2000-01-01", "2010-01-01")))
+  expect_warning(pin_age("101010-101B", date = "1901-01-01"))
+})
+ 
+test_that("pin_sex() works correctly", { 
   expect_true(is.na(pin_sex("010101-010A")))
   expect_false(is.na(pin_sex("010101-0101")))
+})
+
+test_that("rpin() works correctly", {
+  expect_equal(length(rpin(100)), 100)
+  expect_equal(length(rpin(100, p.temp = 0.1)), 100)
+  
 })
